9962dafe7e0fade168cb60f4022157f8967aac1d
wojciechloboda <loboda.w28@gmail.com>
2022-10-17 09:15:04 +0200

    small addition

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -7,6 +7,13 @@ public class World {
         MapDirection dir = MapDirection.NORTH;
         System.out.println(dir.previous().next().toString());
         System.out.println(dir.toUnitVector().toString());
+        System.out.println(" ");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 }
 
